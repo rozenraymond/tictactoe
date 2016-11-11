ee37f92c914a6dd3dcf47f2408612e252a61ffc2
zenrevilene <zen.revilene@gmail.com>
2016-11-11 12:25:11 +1100

    remove balloon images if compWin

--- tictactoe/main.js
+++ tictactoe/main.js
@@ -5,25 +5,25 @@ var player1Score = 0;
 var player2Score = 0;
 
 swal({
-  title: 'Please choose player',
-  showCancelButton: true,
-  confirmButtonColor: '#FFF',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Player One',
-  cancelButtonText: 'Player Two',
-  confirmButtonClass: 'btn btn-success',
-  cancelButtonClass: 'btn btn-danger',
-  buttonsStyling: false
+    title: 'Please choose player',
+    showCancelButton: true,
+    confirmButtonColor: '#FFF',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Player One',
+    cancelButtonText: 'Player Two',
+    confirmButtonClass: 'btn btn-success',
+    cancelButtonClass: 'btn btn-danger',
+    buttonsStyling: false
 }).then(function() {
-  player = "x";
-  computer = "o";
+    player = "x";
+    computer = "o";
 }, function(dismiss) {
-  // dismiss can be 'cancel', 'overlay',
-  // 'close', and 'timer'
-  if (dismiss === 'cancel') {
-    player = "o";
-    computer = "x";
-  }
+    // dismiss can be 'cancel', 'overlay',
+    // 'close', and 'timer'
+    if (dismiss === 'cancel') {
+        player = "o";
+        computer = "x";
+    }
 });
 
 var checkWin;
@@ -38,7 +38,7 @@ var compData = ["one", "two", "three", "four", "five", "six", "seven", "eight", 
 
 var checkPlayerWin = [];
 var checkCompWin = [];
-var turn =0;
+var turn = 0;
 var move = 0;
 
 //function to put input X/O
@@ -76,41 +76,41 @@ var newGame = function() {
             turn = 1;
 
 
-            if(playerWin === true ){
+            if (playerWin === true) {
                 //alert("You win!");
                 fallingSnow(); // Falling snow as reward
                 swal({
-                  title: 'You are the winner',
-                  html: $('<div>')
-                    .addClass('some-class')
-                    .text('Great Job!'),
-                  animation: false,
-                  customClass: 'animated tada'
-                }).then( function () {
+                    title: 'You are the winner',
+                    html: $('<div>')
+                        .addClass('some-class')
+                        .text('Great Job!'),
+                    animation: false,
+                    customClass: 'animated tada'
+                }).then(function() {
                     player1Score++;
-                    $( "#score1" ).html( "Score: " + player1Score );
+                    $("#score1").html("Score: " + player1Score);
                     newGame();
-                  });
+                });
 
                 return; // If the player wins, no need to execute the remaining code
             }
             compMove();
             checkWin();
-            if(computerWin === true){
+            if (computerWin === true) {
                 // alert("Sorry, you lose");
 
-                fallingSnow(); // Falling snow as reward
+                // fallingSnow(); Falling snow as reward
                 swal({
-                  title: 'You lose!',
-                  html: $('<div>')
-                    .addClass('some-class')
-                    .text('Better luck next time!'),
-                  animation: false,
-                  customClass: 'animated tada'
-                }).then( function () {
-                  player1Score++;
-                  $( "#score1" ).html( "Score: " + player1Score );
-                  newGame();
+                    title: 'You lose!',
+                    html: $('<div>')
+                        .addClass('some-class')
+                        .text('Better luck next time!'),
+                    animation: false,
+                    customClass: 'animated tada'
+                }).then(function() {
+                    player1Score++;
+                    $("#score1").html("Score: " + player1Score);
+                    newGame();
                 });
 
                 return; // If the comp wins, no need to execute the remaining code
@@ -119,12 +119,12 @@ var newGame = function() {
             if (move > 9) {
                 // alert("It's a tie!");
                 swal({
-                  title: "It's a tie",
-                  html: $('<div>')
-                    .addClass('some-class')
-                    .text('Better luck next time!'),
-                  animation: false,
-                  customClass: 'animated tada'
+                    title: "It's a tie",
+                    html: $('<div>')
+                        .addClass('some-class')
+                        .text('Better luck next time!'),
+                    animation: false,
+                    customClass: 'animated tada'
                 });
             }
 
@@ -171,8 +171,8 @@ var winningCombinations = [
 var checkWin = function(x) {
 
     for (var i = 0; i < winningCombinations.length; i++) {
-      var playerMatches = 0;
-      var compMatches = 0;
+        var playerMatches = 0;
+        var compMatches = 0;
         // winningCombinations[i] will be one winning combination.
         // Now iterate over all the elements in checkPlayerWin
         for (var j = 0; j < checkPlayerWin.length; j++) {
@@ -199,43 +199,43 @@ var checkWin = function(x) {
 
 
 // Snow Falling
-    function fallingSnow() {
-        var $snowflakes = $(),
-            createSnowflakes = function () {
-                var qt = 100;
-                for (var i = 0; i < qt; ++i) {
-                    var $snowflake = $('<div class="snowflakes"></div>');
-                    $snowflake.css({
-                        'left': (Math.random() * $('#site').width()) + 'px',
-                        'top': (- Math.random() * $('#site').height()) + 'px'
-                    });
-                    // add this snowflake to the set of snowflakes
-                    $snowflakes = $snowflakes.add($snowflake);
-                }
-                $('#snowZone').prepend($snowflakes);
-            },
-
-            runSnowStorm = function() {
-                $snowflakes.each(function() {
-
-                    var singleAnimation = function($flake) {
-                        $flake.animate({
-                            top: "500px",
-                            opacity : "0",
-                        }, Math.random()*-2500 + 5000, function(){
-                            // this particular snow flake has finished, restart again
-                            $flake.css({
-                                'left': (Math.random() * $('#site').width()) + 'px',
-                                'top': (- Math.random() * $('#site').height()) + 'px',
-                                'opacity': 1
-                            });
-                            singleAnimation($flake);
-                        });
-                    };
-                    singleAnimation($(this));
+function fallingSnow() {
+    var $snowflakes = $(),
+        createSnowflakes = function() {
+            var qt = 100;
+            for (var i = 0; i < qt; ++i) {
+                var $snowflake = $('<div class="snowflakes"></div>');
+                $snowflake.css({
+                    'left': (Math.random() * $('#site').width()) + 'px',
+                    'top': (-Math.random() * $('#site').height()) + 'px'
                 });
+                // add this snowflake to the set of snowflakes
+                $snowflakes = $snowflakes.add($snowflake);
+            }
+            $('#snowZone').prepend($snowflakes);
+        },
+
+        runSnowStorm = function() {
+            $snowflakes.each(function() {
+
+                var singleAnimation = function($flake) {
+                    $flake.animate({
+                        top: "500px",
+                        opacity: "0",
+                    }, Math.random() * -2500 + 5000, function() {
+                        // this particular snow flake has finished, restart again
+                        $flake.css({
+                            'left': (Math.random() * $('#site').width()) + 'px',
+                            'top': (-Math.random() * $('#site').height()) + 'px',
+                            'opacity': 1
+                        });
+                        singleAnimation($flake);
+                    });
+                };
+                singleAnimation($(this));
+            });
         };
 
-        createSnowflakes();
-        runSnowStorm();
-    }
+    createSnowflakes();
+    runSnowStorm();
+}
